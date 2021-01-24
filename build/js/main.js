3cc4fdda97422df3f33a85d97c860e2da9339c1d
alisam1 <alisa-m2@yandex.ru>
2021-01-25 01:22:17 +0300

    Summ

---
+++ build/js/main.js
@@ -0,0 +1,25 @@
+/* Mobile menu */
+$(document).ready(function(){
+    if ($(window).width() < 768) {
+        $('.header__nav-button').click(function(){
+            $('.header__list').slideToggle(300);
+            return false;
+        });
+}
+
+});
+
+/* Select */
+$(document).ready(function(){
+    $('.faux-select').click(function(){
+        $(this).toggleClass('open');
+        $('.options',this).toggleClass('open');
+        });
+
+        $('.options li').click(function(){
+        var selection = $(this).text();
+        var dataValue = $(this).attr('data-value');
+        $('.selected-option span').text(selection);
+        $('.faux-select').attr('data-selected-value',dataValue);
+    });
+});
